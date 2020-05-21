0cdfe28a1c802a6f8c109005047e29c53190cf95
Naouali <elnaoualinebil@gmail.com>
2020-05-21 06:12:46 +0100

    model reset

--- app/stocks_api/models.py
+++ app/stocks_api/models.py
@@ -1,23 +1,11 @@
 from django.db import models
-
+import pandas as pd
 # Create your models here.
+
 class Companies(models.Model):
-    number = models.IntegerField(null=False)
+    id = models.IntegerField(primary_key=True, auto_created=True)
     name = models.CharField(max_length=20, null=False)
 
     def __str__(self):
-        return self.name
-class Data(models.Model):
-    id = models.ForeignKey(Companies)
-    date = models.DateField()
-    price = models.IntegerField(null=False)
-    opening_price = models.IntegerField(null=False)
-    low = models.IntegerField(null=False)
-    high = models.IntegerField(null=False)
-    volume = models.IntegerField(null=False)
-    variation = models.IntegerField()
-
-
-    def __str__(self):
-        return 
+        return str(self.id) + "--" + self.name
 
